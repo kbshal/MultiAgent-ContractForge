44c40f7aeb3b3d1b7fbc5130d0cb8ea0d8e28d83
kbshal <bishalkharel0000@gmail.com>
2024-06-01 11:15:35 +0545

    feat: add employee model

---
+++ src/models/employee.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr, constr
+
+
+class Employee(BaseModel):
+    FirstName: constr(min_length=1)
+    MiddleName: str = None
+    LastName: constr(min_length1=1)
+    Email: EmailStr
+    countryOfCitizenship: dict
+    countryOfWork: str
+    jobTitle: constr(min_length=1)
+    scopeWork: str
+
+
+
