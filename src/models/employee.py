038ca7da9999259bda8823f7b4e52293077affd1
kbshal <bishalkharel0000@gmail.com>
2024-06-01 11:30:27 +0545

    fix: typo fix in employee model

--- src/models/employee.py
+++ src/models/employee.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, EmailStr, constr
 class Employee(BaseModel):
     FirstName: constr(min_length=1)
     MiddleName: str = None
-    LastName: constr(min_length1=1)
+    LastName: constr(min_length=1)
     Email: EmailStr
     countryOfCitizenship: dict
     countryOfWork: str
