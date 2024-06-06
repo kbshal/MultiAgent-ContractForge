c8bb0f4efe0b77a6477ade3358fd437fa80a1318
kbshal <bishalkharel0000@gmail.com>
2024-06-06 22:47:28 +0545

    fix: bug fix in agentlogger [passed the essential arguments]

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -2,8 +2,8 @@ from loguru import logger
 
 
 class AgentLogger(logger):
-    def __init__(self):
-        super().__init__()
+   def __init__(self, record=None, lazy=False, colors=True, raw=False, capture=False, patchers=None, extra=None):
+        super().__init__(record=record, lazy=lazy, colors=colors, raw=raw, capture=capture, patchers=patchers, extra=extra)
 
         self.level("DEBUG_INFO", no=15)
 
