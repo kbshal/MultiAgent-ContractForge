9a29af4a5eed4ee633298ae4a144cbae11b97a05
kbshal <bishalkharel0000@gmail.com>
2024-06-06 22:44:54 +0545

    chore: add dependency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.routers import emp_gen_info
-
+import uvicorn
 
 app = FastAPI()
 
@@ -9,3 +9,7 @@ app.include_router(
     emp_gen_info.router,
     tags=['store general info']
 )
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=2939)
